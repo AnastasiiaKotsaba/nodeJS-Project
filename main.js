da7eee836227fd782c6e5c100e3842cfe85fff3a
AnastasiiaKotsaba <anastasiakocaba49@gmail.com>
2025-09-12 19:33:11 +0300

    was iniatialized new nodeJS project

---
+++ main.js
@@ -0,0 +1,26 @@
+let moment = require('moment')
+
+// 1
+function getCurrentDate() {
+    console.log(`Today is: ${moment().format('dddd')}`)
+}
+
+// 2
+function getCurrentMonth() {
+    console.log(`Current month is: ${moment().format('MMMM')}`)
+}
+
+// 3
+function getCurrentYear() {
+    console.log(`Current year is: ${moment().format('YYYY')}`)
+}
+
+// 4
+function getDate(){
+    console.log(`Current date is: ${moment().format('YYYY/MM/DD HH:mm:ss')}`)
+}
+
+getCurrentDate()
+getCurrentMonth()
+getCurrentYear()
+getDate()
