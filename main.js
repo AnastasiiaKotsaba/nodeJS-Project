71a6cb1bef36f3d89ce45c14bb37617634d18ac4
AnastasiiaKotsaba <anastasiakocaba49@gmail.com>
2025-09-20 00:14:32 +0300

    was created express app that shows current date

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 let moment = require('moment')
+let express = require('express')
+
+const HOST = '127.0.0.1'
+const PORT = 8000
+
+const app = express()
+
+// Get date JSON responce
+app.get('/timestamp', (req, res) => {
+    res.status(200).json({"Current date": getDate()})
+})
 
 // 1
 function getCurrentDate() {
@@ -18,12 +29,17 @@ function getCurrentYear() {
 // 4
 function getDate(){
     console.log(`Current date is: ${moment().format('YYYY/MM/DD HH:mm:ss')}`)
+    return moment().format('YYYY/MM/DD HH:mm:ss')
 }
 
 // 5
 function getCurrentWeekDay() {
     console.log(`Today is: ${moment().format('dddd')}`)
 }
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server: http://${HOST}:${PORT}`)
+})
 
 getCurrentDate()
 getCurrentMonth()
